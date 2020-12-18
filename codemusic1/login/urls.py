027832c65f03668bf4c8aca67ffd970bb1ca872d
Rithesh <ritheshrai5@gmail.com>
2020-12-18 19:38:35 +0530

    commit

--- codemusic1/login/urls.py
+++ codemusic1/login/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import RedirectView
 from django.conf.urls import url
 urlpatterns = [
     path('', views.index,name='index'),
-    path('register',views.register,name='register'),
-    path('signin',views.signin,name='signin'),
+    path('/register',views.register,name='register'),
+    path('/signin',views.signin,name='signin'),
     url(r'^favicon\.ico$',RedirectView.as_view(url='/static/musicplayer/image/PePer-ICON.png')),
 ]
