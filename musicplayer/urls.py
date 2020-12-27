ad89ccc5d56fd06a9b1542b277bdda8f1691ae23
Rithesh <ritheshrai5@gmail.com>
2020-12-27 15:33:23 +0530

    commit

--- musicplayer/urls.py
+++ musicplayer/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.home,name='home'),
     path('song',views.song,name='song'),
     path('api',views.api,name='api'),
+    path('like',views.like,name='like'),
     path('logout',views.logout,name='logout'),
     url(r'^favicon\.ico$',RedirectView.as_view(url='/static/musicplayer/image/PePer-ICON.png')),
 ]
--- musicplayer/views.py
+++ musicplayer/views.py
@@ -33,16 +33,15 @@ def home(request):
     return render(request ,'musicplayer/home.html') 
 def song(request):
     v1 =request.GET['search']
-    
     response = requests.get('https://codemusic.vercel.app/search?query='+v1)
     geodata = response.json() 
     print(response)
     return HttpResponse( response, content_type='application/json')
-    #return render(request,'musicplayer\\songs.html',{'data':geodata})
 def logout(request):
     auth.logout(request)
     return redirect('/')
 def like(request):
+    #userid,songid,
     if request.method=='GET':
         if music.objects.filter(user=request.POST['userid']).exists():
             musics=music.objects.get(uid=request.POST['userid'])
@@ -51,9 +50,10 @@ def like(request):
             k=to_dict(songlist)
             playist=playlist.objects.get(id=k['liked'])
             n=to_arr(playlist.songList)
-            n.append(request.POST['songid'])
-            playlist.songList=list_to_str(n)
-            playist.save()
+            if request.POST['songid'] not in n:
+                n.append(request.POST['songid'])
+                playlist.songList=list_to_str(n)
+                playist.save()
         else:
             new=request.POST['songid']
             n=list_to_str(new)
@@ -80,9 +80,10 @@ def api(request):
         return HttpResponse( datas, content_type='application/json')
     else:
         response = requests.get('https://codemusic.vercel.app/song?id='+data)
-        songcred =response.json()
-        obj=songs(musicID=songcred['id'], musicTitle=songcred['song'], musicDESC=songcred['primary_artists'],musicIMG=songcred['image'],musicUrl=songcred['media_url'])
-        obj.save()
-        datas = serializers.serialize('json',songs.objects.all().filter(musicID__exact=data))
-        return HttpResponse( datas, content_type='application/json')
-        
+        if response.status_code==200:
+            songcred =response.json()
+            obj=songs(musicID=songcred['id'], musicTitle=songcred['song'], musicDESC=songcred['primary_artists'],musicIMG=songcred['image'],musicUrl=songcred['media_url'])
+            obj.save()
+            datas = serializers.serialize('json',songs.objects.all().filter(musicID__exact=data))
+            return HttpResponse( datas, content_type='application/json')
+    #return render(request,'musicplayer\\songs.html',{'data':geodata})
