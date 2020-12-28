89b2feb3ab3ac7771a1d4660fcde05dd913ca3bb
Rithesh <ritheshrai5@gmail.com>
2020-12-28 22:23:08 +0530

    commit

--- musicplayer/static/musicplayer/js/webapp.js
+++ musicplayer/static/musicplayer/js/webapp.js
@@ -1,4 +1,4 @@
-var bkg= document.getElementById('good');
+
 var len=0,cursong=0;
 var isPlaying = false;
 let songarray=["none"]
@@ -11,8 +11,8 @@ function loadXMLDoc() {
  // xhttp.open("GET", "https://codemusic-django.herokuapp.com/dashboard/song?search="+x, true);
  
  if(Boolean(x)){
-  xhttp.open("GET", "https://codemusic-django.herokuapp.com/dashboard/song?search="+x, true);
-  //xhttp.open("GET", "http://127.0.0.1:8000/dashboard/song?search="+x, true);
+  //xhttp.open("GET", "https://codemusic-django.herokuapp.com/dashboard/song?search="+x, true);
+  xhttp.open("GET", "http://127.0.0.1:8000/dashboard/song?search="+x, true);
   xhttp.send();
   xhttp.onload = () => {
   	if(xhttp.status==200){
@@ -33,7 +33,6 @@ for (i = 0; i < len; i++)
   	document.getElementById("demo").innerHTML=god+"</div>";
   }
 }}}
-console.log(songarray)
 function playSong(kid){
  var xhttp2 = new XMLHttpRequest();
  xhttp2.open("GET", "https://codemusic.vercel.app/song?id="+kid, true);
@@ -73,8 +72,8 @@ function djangosong(id)
 { 
   cursong=songarray.indexOf(id);
   var xhttp3 = new XMLHttpRequest();
-   xhttp3.open("GET", "https://codemusic-django.herokuapp.com/dashboard/api?id="+id, true);
-   //xhttp3.open("GET", "http://127.0.0.1:8000/dashboard/api?id="+id, true);
+   //xhttp3.open("GET", "https://codemusic-django.herokuapp.com/dashboard/api?id="+id, true);
+   xhttp3.open("GET", "http://127.0.0.1:8000/dashboard/api?id="+id, true);
  xhttp3.send();
   xhttp3.onload = () => {
   	if(xhttp3.status==200){
@@ -87,7 +86,9 @@ function djangosong(id)
       document.getElementById("songinfo").innerHTML="<h1>"+h[0].fields.musicTitle+"</h1><p>"+h[0].fields.musicDESC+"</p>";
       //<audio id='myAudio'  ' hidden="hidden"><source src='${h[0].fields.musicUrl}' type='audio/mpeg'>  Your browser does not support the audio tag.</audio>`;
       playss();
-     // bkg.style.backgroundImage = "url('"+ h[0].fields.musicIMG+"') !importent";
+      //document.querySelector(".codeplay").style.backgroundImage = 'url("https://source.unsplash.com/1600x900/?music,peace")';
+      //document.querySelector(".codeplay").style.backgroundImage = "url('"+ h[0].fields.musicIMG+"') ";
+      document.querySelector(".body").style.backgroundImage = "url('"+ h[0].fields.musicIMG+"') ";
      // document.getElementById("plybtn").style.display = "none";
       //document.getElementById("psbtn").style.display = "initial" ;
 
@@ -111,3 +112,33 @@ document.addEventListener("keyup", function(event) {
   }
 });
 //player
+function initiallaod() {
+  var x = "namo"
+  var xhttp = new XMLHttpRequest();
+  var good=" ",god="";
+ // xhttp.open("GET", "https://codemusic-django.herokuapp.com/dashboard/song?search="+x, true);
+ 
+ if(Boolean(x)){
+  //xhttp.open("GET", "https://codemusic-django.herokuapp.com/dashboard/song?search="+x, true);
+  xhttp.open("GET", "http://127.0.0.1:8000/dashboard/song?search="+x, true);
+  xhttp.send();
+  xhttp.onload = () => {
+    if(xhttp.status==200){
+      var k = JSON.parse(xhttp.response)
+      console.log(k);
+      if(k.Result=='false'){
+      god ="<h1>No Results Found For now </h1>";}
+      else
+      { god="<div class='grid'>"
+        len=k.length;
+for (i = 0; i < len; i++) 
+    {
+      good =`<button type='button' class='btn btn-light full' onclick='djangosong(\"${k[i].id}\")'><div><img src='${k[i].image}' alt='${k[i].title}' width=141 height=141><h1>${k[i].title}</h1><p>${k[i].description}</p></div></button>`;
+      god=god+good;
+      songarray[i]=k[i].id
+    }
+  }
+    document.getElementById("demo").innerHTML=god+"</div>";
+  }
+}}}
+initiallaod();
