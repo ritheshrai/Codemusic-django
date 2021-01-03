9ce31b344735aa45482582b04a05a0732c156b20
Rithesh <ritheshrai5@gmail.com>
2021-01-03 12:27:44 +0530

    commit

--- musicplayer/admin.py
+++ musicplayer/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import songs, music,playlistCON ,playlist
 # Register your models here.
 admin.site.register(songs)
-admin.site.register(music)
-admin.site.register(playlistCON )
-admin.site.register(playlist)
+#admin.site.register(music)
+#admin.site.register(playlistCON )
+#admin.site.register(playlist)
